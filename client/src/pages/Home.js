ef9037bc3c8ce12027b130b13e95d3fac6087de4
Matt <34251520+mwoof@users.noreply.github.com>
2020-06-27 17:52:43 -0400

    Core routes added

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+//Styling
+// import "./BannerStyle.css";
+
+class Banner extends Component {
+  render() {
+    return <div>Hello</div>;
+  }
+}
+
+export default Banner;
