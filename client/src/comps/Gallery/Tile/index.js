4c54fc4fe65b8b707abc709923841db6231472d0
Matt <34251520+mwoof@users.noreply.github.com>
2020-06-30 14:16:16 -0400

    first draft of gallery component

---
+++ client/src/comps/Gallery/Tile/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./indexStyle.css";
+
+import Image from "../../../media/images/Img1.png";
+
+const Drop = (props) => {
+  const link = "project1";
+  const name = "Project 1";
+  const image = Image;
+
+  return (
+    <Link to={`/projects/${link}`} className="tile-cont flex-cent">
+      <img src={image} />
+      <div className="tile-overlay flex-cent flex-col">
+        <h1>{name}</h1>
+      </div>
+    </Link>
+  );
+};
+
+export default Drop;
--- client/src/comps/Gallery/index.js
+++ client/src/comps/Gallery/index.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 
+import "./indexStyle.css";
+
+import Tile from "./Tile";
+
 class Gallery extends Component {
   render() {
-    return <div className="gal-cont">Gallery</div>;
+    const tileSet = [1, 2, 3, 4, 5];
+    const tiles = tileSet.map((tile) => <Tile />);
+
+    return <div className="gal-cont spacing">{tiles}</div>;
   }
 }
 
