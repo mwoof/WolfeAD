961d7068c28e6782e92031645eeb765b53e8032e
Matt <34251520+mwoof@users.noreply.github.com>
2020-07-15 14:22:30 -0500

    updates about and SVG icons/links

--- client/src/comps/Section/AboutText/index.js
+++ client/src/comps/Section/AboutText/index.js
@@ -3,29 +3,37 @@ import React from "react";
 import Line from "../../Line";
 import Highlight from "./Highlight";
 
-const Section = (props) => {
+const Section = props => {
   return (
     <div>
       <h1>Architecture & Design</h1>
-      <p>
-        Wolfe Architecture & Design is founded on analysis and exploration. We
-        relish the challenges inherent across a wide range of work, engaging our
-        expertise and agility to deliver lasting, meaningful design that brings
-        value and joy to clients and users.
+      <p style={{ marginBottom: "36px" }}>
+        Wolfe A&D is an architecture and design firmed based in St. Louis
+        Missouri. Founded in 2012, the practice operates worldwide with projects
+        in Beijing, Normandy, Yunnan, and major cities across the United States.
         <br />
         <br />
-        Founded in 2012, the practice operates worldwide with projects in
-        Beijing, Normandy, Yunnan, and major cites across the United States. As
-        an international practice Wolfe A&D covers architecture, planning and
-        industrial design across all major sectors.
+        As an international practice Wolfe, A&D covers architecture, planning
+        and industrial design across all major sectors.
       </p>
       <div className="Highlight-cont">
         <Highlight number="40" unit="y" text="Experience" />
         <Highlight number="60" unit="+" text="Projects" />
-        <Highlight number="40" unit="y" text="Years of experience" />
+        <Highlight number="??" unit="?" text="??????" />
       </div>
     </div>
   );
 };
 
 export default Section;
+
+// Wolfe Architecture & Design is founded on analysis and exploration. We
+// relish the challenges inherent across a wide range of work, engaging our
+// expertise and agility to deliver lasting, meaningful design that brings
+// value and joy to clients and users.
+// <br />
+// <br />
+// Founded in 2012, the practice operates worldwide with projects in
+// Beijing, Normandy, Yunnan, and major cites across the United States. As
+// an international practice Wolfe A&D covers architecture, planning and
+// industrial design across all major sectors.
