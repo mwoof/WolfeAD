1f418f741dda291991cf6f88b06382b50b22d270
Matt <34251520+mwoof@users.noreply.github.com>
2020-07-17 12:18:38 -0400

    not entierly sure what this is but it affects the carrosel

--- client/src/comps/SectionCarousel/ProjectMedia/ProjectMediaSlide/index.js
+++ client/src/comps/SectionCarousel/ProjectMedia/ProjectMediaSlide/index.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Section = props => {
   const name = props.data.name;
   return (
-    <div className="car-sec-slide">
+    <div className="car-sec-slide" style={{ overflow: "hidden" }}>
       <Link
         to={`/projects/project/${props.id}/${
           name && name.includes(" ") ? name.replace(/\s+/g, "-") : name
